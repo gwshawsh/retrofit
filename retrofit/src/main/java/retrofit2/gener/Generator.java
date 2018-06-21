ebc045e5861d3d6f24744371bfe466ce79f13817
gwshawsh <gwshawsh@163.com>
2018-06-21 17:57:19 +0800

    gener

---
+++ retrofit/src/main/java/retrofit2/gener/Generator.java
@@ -0,0 +1,8 @@
+package retrofit2.gener;
+
+import java.util.Map;
+
+
+public interface Generator {
+    String generate(Map<String, String> extendFields);
+}
---
+++ retrofit/src/main/java/retrofit2/gener/MapGenerator.java
@@ -0,0 +1,7 @@
+package retrofit2.gener;
+
+import java.util.Map;
+
+public interface MapGenerator {
+    Map<String, String> generate(Map<String, String> extendFields);
+}
